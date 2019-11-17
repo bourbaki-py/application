0c0eeaae4f2d5fb1e9e6f862d8ba7b448860ea3f
mattHawthorn <hawthorn.matthew@gmail.com>
2019-11-17 00:52:52 -0500

    test script for nested args in tests/

---
+++ tests/foo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from bourbaki.application.cli import CommandLineInterface, ArgSource
+from typing import *
+
+cli = CommandLineInterface(
+    prog="foo.py",
+    arg_lookup_order=(ArgSource.CLI, ArgSource.DEFAULTS),
+)
+
+
+@cli.definition
+class Foo:
+    """command line interface called foo"""
+
+    def __init__(self, x: int = 42, y: Optional[List[bool]] = None):
+        """
+        set it up
+
+        :param x: an int
+        :param y: a list of bools
+        """
+        self.x = x
+        self.y = y
+
+    def wut(self, tup: Tuple[Tuple[int, int], str, Tuple[complex, ...]], opt: Optional[List[Set[int]]]=None):
+        """
+        wut to the wut
+        :param tup: crazy nested tuple
+        :param opt: nested lists
+        :return:
+        """
+        print(tup)
+        print(opt)
+
+
+if __name__ == "__main__":
+    cli.run()
