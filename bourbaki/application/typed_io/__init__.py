23bd2af4d3c246b427abba2c3010824b7f8b37ef
mattHawthorn <hawthorn.matthew@gmail.com>
2020-03-19 23:36:56 -0400

    reorganize imports in typed_io top level init

--- bourbaki/application/typed_io/__init__.py
+++ bourbaki/application/typed_io/__init__.py
@@ -1,14 +1,12 @@
-# coding:utf-8
+# # coding:utf-8
 from .main import TypedIO, ArgSource, CLI, CONFIG, ENV, STDIN
-from .cli_nargs_ import cli_nargs, cli_option_nargs, cli_action
-from .cli_complete import cli_completer
-from .cli_repr_ import cli_repr
-
-# import this last because it can register default implementations for the others
-from .cli_parse import cli_parser
-from .config_encode import config_encoder, config_key_encoder
-from .config_decode import config_decoder, config_key_decoder
-from .config_repr_ import config_repr
-from .env_parse import env_parser
-from .stdin_parse import stdin_parser
-from .utils import File
+from .cli.cli_nargs_ import cli_nargs
+from .cli.cli_repr_ import cli_repr
+from .cli.cli_complete import cli_completer
+from .cli.cli_parse import cli_parser
+from .config.config_encode import config_encoder
+from .config.config_decode import config_decoder
+from .config.config_repr_ import config_repr
+from .env.env_parse import env_parser
+from .stdin.stdin_parse import stdin_parser
+from .file_types import File
